e70f65d74fa9b612f8652b44647d9e409119238e
Muhammad Farhan <mfarhan.jp@gmail.com>
2021-03-20 23:12:21 +0500

    Add: License and Handle API Key

--- app/src/main/java/com/farhan/pixo/arch/api/PixabayApi.kt
+++ app/src/main/java/com/farhan/pixo/arch/api/PixabayApi.kt
@@ -1,5 +1,19 @@
+/**Designed and developed by 2021 Muhammad Farhan
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.*/
 package com.farhan.pixo.arch.api
 
+import com.farhan.pixo.BuildConfig
 import com.farhan.pixo.model.Image
 import retrofit2.http.GET
 import retrofit2.http.Query
@@ -13,7 +27,7 @@ interface PixabayApi {
     @GET("api/")
     suspend fun getImages(
         @Query("q") query: String = "",
-        @Query("key") key: String = "20674327-3911c1290f7b8097d85649f8d",
+        @Query("key") key: String = BuildConfig.PIXABAY_API_KEY,
         @Query("image_type") imageType: String = "photo",
         @Query("page") page: Int = 1,
         @Query("per_page") perPage: Int = 20
--- app/src/main/java/com/farhan/pixo/ui/gallery/paging/PixabayPagingSource.kt
+++ app/src/main/java/com/farhan/pixo/ui/gallery/paging/PixabayPagingSource.kt
@@ -1,12 +1,23 @@
-package com.farhan.pixo.ui.gallery.paging
+/**Designed and developed by 2021 Muhammad Farhan
 
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.*/
+package com.farhan.pixo.ui.gallery.paging
 
 import androidx.paging.PagingSource
 import androidx.paging.PagingState
 import com.farhan.pixo.arch.api.PixabayApi
 import com.farhan.pixo.model.Images
 import timber.log.Timber
-
 
 private const val PIXABAY_STARTING_PAGE_INDEX = 1
 
